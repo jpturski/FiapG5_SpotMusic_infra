24b9a4bc3a4e99bd36012fd281b280fafc893f82
celsoraimundo <celsoraimundo@users.noreply.github.com>
2023-05-06 11:48:37 -0300

    Atualiza resources

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,45 +1,15 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "backendvm" {
-  name         = "fiap5-spotmusic-frontend"
-  machine_type = "n1-standard-1"
-  zone         = var.zone
 
-  # Define a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20230213"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
-  }
+resource "google_artifact_registry_repository" "fiap5-frontend-repo" {
+  location = var.region
+  repository_id = "FiapG5_SpotMusic_backend"
+  description = "Imagens Docker"
+  format = "DOCKER"
 }
 
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "frontendvm" {
-  name         = "fiap5-spotmusic-frontend"
-  machine_type = "n1-standard-1"
-  zone         = var.zone
 
-  # Define a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20230213"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
-  }
+resource "google_artifact_registry_repository" "fiap5-backend-repo" {
+  location = var.region
+  repository_id = "FiapG5_SpotMusic_frontend"
+  description = "Imagens Docker"
+  format = "DOCKER"
 }
